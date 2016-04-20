cc24cc4fae95cf0c9db8b9158cb5c6f655664463
Jewei Mak <jewei.mak@gmail.com>
2016-04-20 13:46:47 +0800

    Create update-ghost.sh

---
+++ update-ghost.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Written by Jewei Mak <jewei.mak@gmail.com> for Digital Ocean Ghost (one-click-install) to the latest version.
+
+# Stop Ghost.
+service ghost stop
+
+# Goto working dir.
+cd /var/www/ghost
+
+# Backup database.
+for file in /var/www/ghost/content/data/*.db;
+  do cp "$file" "${file}-backup-`date +%Y%m%d`";
+done
+
+# Clear dir and file.
+rm -rf /var/www/ghost/ghost-temp /var/www/ghost/ghost-latest.zip /var/www/ghost/latest.zip
+
+# Get lastest package.
+curl -o /var/www/ghost/latest.zip -Lk https://ghost.org/zip/ghost-latest.zip
+unzip /var/www/ghost/latest.zip -d /var/www/ghost/ghost-temp
+
+# Remove the core directory completely.
+rm -rf /var/www/ghost/core
+
+# Copy the new files over.
+yes | cp -R /var/www/ghost/ghost-temp/core /var/www/ghost
+yes | cp /var/www/ghost/ghost-temp/*.js /var/www/ghost
+yes | cp /var/www/ghost/ghost-temp/*.json /var/www/ghost
+yes | cp /var/www/ghost/ghost-temp/*.md /var/www/ghost
+yes | cp -R /var/www/ghost/content/themes/casper /var/www/ghost/content/themes
+
+# Delete temp folder.
+rm -rf /var/www/ghost/ghost-temp /var/www/ghost/latest.zip
+
+# Set permissions.
+chown -R ghost:ghost /var/www/ghost
+
+# Upgrade dependencies:
+npm install --production
+
+# Start Ghost.
+service restart ghost
+echo "Ghost started."
